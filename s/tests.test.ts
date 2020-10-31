7b4158e0dad25d5ffebab081a604e0122683ae6a
Chase Moskal <chasemoskal@gmail.com>
2020-10-31 16:16:40 -0700

    rename: feature-stories.test.ts

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -1,8 +1,8 @@
 
 import {Suite} from "cynic"
-import pay from "./features/pay/pay.test.js"
+import pay from "./features/pay/pay-stories.test.js"
 import dbby from "./toolbox/dbby/dbby.test.js"
-import core from "./features/core/core.test.js"
+import core from "./features/core/core-stories.test.js"
 
 export default <Suite>{
 	"toolbox": {dbby},
