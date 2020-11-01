5fb93e3dde7bd6948301244c7f30b08df3f487e9
Chase Moskal <chasemoskal@gmail.com>
2020-10-31 17:48:03 -0700

    add: antagonizing test

--- s/features/core/core-stories.test.ts
+++ s/features/core/core-stories.test.ts
@@ -1,19 +1,33 @@
 
 import {Suite, expect} from "cynic"
 
+import {and} from "../../toolbox/dbby/dbby-memory.js"
 import {wholeSystemTestable} from "../../assembly/whole-system-testable.js"
+import { prepareConstrainTables } from "../../toolbox/dbby/dbby-constrain.js"
+
+// make a test case return true
+const rt = (f: () => Promise<any>) => async() => !await f()
 
 export default <Suite>{
 	"technician": {
-		"login and out of platform": async() => {
+		"login and out of platform": rt(async() => {
 			const system = await wholeSystemTestable()
 			const {core} = system.frontend.models
+
 			await core.login()
 			expect(core.user).ok()
+			const userId = core.user.id
+
 			await core.logout()
 			expect(core.user).not.ok()
-			return true
-		},
+
+			const constrainTables = prepareConstrainTables(system.backend.tables)
+			const tables = constrainTables({appId: system.platformAppId})
+			const count = await tables.account.count({
+				conditions: and({equal: {userId}})
+			})
+			expect(count === 1).ok()
+		}),
 		"view platform stats": true,
 		"manage apps": true,
 		"procotol zero: roll platform secrets": true,
