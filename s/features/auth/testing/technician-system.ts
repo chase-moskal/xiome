c96123de0f4a2870e354c47bc232b57f84c12d24
Chase Moskal <chasemoskal@gmail.com>
2020-11-23 17:19:47 -0800

    tweak: whitespace

--- s/features/auth/testing/technician-system.ts
+++ s/features/auth/testing/technician-system.ts
@@ -6,11 +6,13 @@ export async function technicianSystem() {
 	const appToken = testable.platformAppToken
 	const {loginTopic} = testable.system.backend.authApi
 	const {email} = testable.system.config.platform.technician
+
 	testable.system.controls.setNextLogin(async function registerTechnician() {
 		const {nextLoginEmail} = testable
 		await loginTopic.sendLoginLink({appToken}, {email})
 		const {loginToken} = await nextLoginEmail
 		return loginTopic.authenticateViaLoginToken({appToken}, {loginToken})
 	})
+
 	return testable
 }
