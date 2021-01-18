a1d2ce2417a742630da4e105887c3bf1dbf22fe2
Chase Moskal <chasemoskal@gmail.com>
2021-01-18 14:09:38 -0800

    tweak: name in mobx model mixin

--- s/framework/mixin-model-mobx.ts
+++ s/framework/mixin-model-mobx.ts
@@ -5,7 +5,7 @@ import {ConstructorFor} from "../types.js"
 type MixinIn = ConstructorFor<any>
 
 export function mixinModelMobx<C extends MixinIn>(Constructor: C): C {
-	return class LitElementWithMobxAutorun extends Constructor {
+	return class Observable extends Constructor {
 		constructor(...args: any[]) {
 			super(...args)
 			makeObservable(this)
