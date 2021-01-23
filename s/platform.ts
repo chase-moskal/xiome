02ee9bb0eb700939037b78770254c5d0cff99074
Chase Moskal <chasemoskal@gmail.com>
2021-01-22 22:44:40 -0800

    fix: platform frontend ssg

--- s/platform.ts
+++ s/platform.ts
@@ -4,7 +4,7 @@ import {registerComponents, themeComponents} from "./framework/component.js"
 
 import theme from "./theme.css.js"
 
-void async function demo() {
+void async function platform() {
 	registerComponents(themeComponents(theme, {
 		FarmExample,
 	}))
