129e189a2dc7c3b1451db18a9c4c5071977467dc
Chase Moskal <chasemoskal@gmail.com>
2021-01-27 14:58:01 -0800

    tweak: platform app name

--- s/features/auth/tests/helpers/constants.ts
+++ s/features/auth/tests/helpers/constants.ts
@@ -1,5 +1,5 @@
 
-export const platformAppLabel = "Xiome Apps"
+export const platformAppLabel = "Xiome Platform"
 export const platformLink = "https://xiome.io/"
 export const apiLink = "https://api.xiome.io/"
 export const technicianEmail = "chase@xiome.io"
