458317fb74f35b5687b11b69dac0a076d366e2d9
Chase Moskal <chasemoskal@gmail.com>
2021-01-27 01:41:15 -0800

    rename: xiom to xiome

--- s/features/auth/tests/helpers/constants.ts
+++ s/features/auth/tests/helpers/constants.ts
@@ -1,6 +1,6 @@
 
-export const platformAppLabel = "Xiom Apps"
-export const platformLink = "https://xiom.app/"
-export const apiLink = "https://api.xiom.app/"
-export const technicianEmail = "chasemoskal@gmail.com"
+export const platformAppLabel = "Xiome Apps"
+export const platformLink = "https://xiome.io/"
+export const apiLink = "https://api.xiome.io/"
+export const technicianEmail = "chase@xiome.io"
 export const appLink = "https://example.chasemoskal.com/"
