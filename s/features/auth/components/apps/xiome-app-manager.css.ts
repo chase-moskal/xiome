c998966c15c67e82a58075269f137094682dbea6
Chase Moskal <chasemoskal@gmail.com>
2021-01-31 02:06:57 -0800

    rework: padding

--- s/features/auth/components/apps/xiome-app-manager.css.ts
+++ s/features/auth/components/apps/xiome-app-manager.css.ts
@@ -3,7 +3,8 @@ import {css} from "../../../../framework/component.js"
 export default css`
 
 .applist .app {
-	padding: 1rem;
+	padding: 0.4em 1rem;
+	margin: 0 var(--xmargin, 0);
 	margin-top: 0.5em;
 	border: 1px solid;
 }
@@ -13,7 +14,8 @@ export default css`
 }
 
 .appcreator {
-	padding: 1rem;
+	margin: 0 var(--xmargin, 0);
+	padding: 0.4em 1rem;
 }
 
 `
