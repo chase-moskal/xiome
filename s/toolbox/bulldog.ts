cc9cf33745799f03f0e96385db6e13bec271980c
Chase Moskal <chasemoskal@gmail.com>
2021-02-14 18:56:34 -0800

    add: interesting 'bulldog' templating concept

---
+++ s/toolbox/bulldog.ts
@@ -0,0 +1,35 @@
+
+/*
+
+// concept: a lit-html replacement
+
+function render() {
+	return bulldog`
+
+		// supports comments
+		<h1> indents instead of closing tags
+
+		<p>
+			this is content,
+			and it wraps
+
+		<ul>
+			<li> item 1
+			<li> item 2
+
+		<div attributes=easy>
+
+		<xio-text-input
+			?booleans=${truthy()}
+			.properties=${justLikeLit()}
+			@click=${eventHandlersToo}
+			...${spreadAttributes}
+			....${spreadProps}
+			?...${spreadBools}
+			@...${spreadHandlers}>
+				inner content
+
+	`
+}
+
+*/
