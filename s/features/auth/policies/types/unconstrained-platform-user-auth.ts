6b61ad1fb5f4057411ba1460b775c19bf06ba0f8
Chase Moskal <chasemoskal@gmail.com>
2021-03-02 03:36:44 -0800

    delete: unconstrained platform user policy types

--- s/features/auth/policies/types/unconstrained-platform-user-auth.ts
+++
@@ -1,7 +0,0 @@
-
-import {PlatformUserAuth} from "./platform-user-auth.js"
-import {AuthTables} from "../../tables/types/auth-tables.js"
-
-export interface UnconstrainedPlatformUserAuth extends PlatformUserAuth {
-	bakeTables: (appId: string) => Promise<AuthTables>
-}
--- s/features/auth/policies/types/unconstrained-platform-user-meta.ts
+++
@@ -1,4 +0,0 @@
-
-import {PlatformUserMeta} from "./platform-user-meta.js"
-
-export interface UnconstrainedPlatformUserMeta extends PlatformUserMeta {}
