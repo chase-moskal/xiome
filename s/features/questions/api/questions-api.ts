1322f03b824425452c7fd756d39976174f387d43
Chase Moskal <chasemoskal@gmail.com>
2021-04-28 17:58:03 -0700

    fix: imports

--- s/features/questions/api/questions-api.ts
+++ s/features/questions/api/questions-api.ts
@@ -1,12 +1,12 @@
 
-import {apiContext} from "renraku/x/api/api-context"
 import {Rando} from "../../../toolbox/get-rando.js"
-import {prepareAuthPolicies} from "../../auth/policies/prepare-auth-policies.js"
-import {questionModerationTopic} from "../topics/question-moderation-topic.js"
+import {apiContext} from "renraku/x/api/api-context.js"
+import {QuestionsTables} from "./tables/types/questions-tables.js"
 import {questionPostingTopic} from "../topics/question-posting-topic.js"
 import {questionReadingTopic} from "../topics/question-reading-topic.js"
+import {questionModerationTopic} from "../topics/question-moderation-topic.js"
+import {prepareAuthPolicies} from "../../auth/policies/prepare-auth-policies.js"
 import {QuestionModeratorAuth, QuestionModeratorMeta, QuestionPosterAuth, QuestionPosterMeta, QuestionReaderAuth, QuestionReaderMeta, questionsPolicies} from "./questions-policies.js"
-import {QuestionsTables} from "./tables/types/questions-tables.js"
 
 export function questionsApi({rando, questionsTables, authPolicies}: {
 	rando: Rando
--- s/features/questions/topics/validation/validate-question-draft.ts
+++ s/features/questions/topics/validation/validate-question-draft.ts
@@ -1,6 +1,6 @@
 
-import {maxLength, minLength, notWhitespace, one, schema, string} from "../../../../toolbox/darkvalley.js"
 import {QuestionDraft} from "../types/question-draft.js"
+import {maxLength, minLength, notWhitespace, one, schema, string} from "../../../../toolbox/darkvalley.js"
 
 export const validateQuestionDraft = schema<QuestionDraft>({
 	board: one(string(), minLength(1), maxLength(32), notWhitespace()),
