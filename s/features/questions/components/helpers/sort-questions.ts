adac81bfd4c97f466aee79eb46ca4ab681ef3812
Chase Moskal <chasemoskal@gmail.com>
2021-06-04 19:19:11 -0700

    fix: questions sort own questions top

--- s/features/questions/components/helpers/sort-questions.ts
+++ s/features/questions/components/helpers/sort-questions.ts
@@ -1,11 +1,12 @@
 
 import {Question} from "../../api/types/question.js"
 
-export function sortQuestions(questions: Question[], myUserId: string) {
+export function sortQuestions(questions: Question[], myUserId?: string) {
 	return [...questions].sort((a, b) => {
 		const promote = {a: -1, b: 1}
 
-		if (a.authorUserId === myUserId) return promote.a
+		if (myUserId && a.authorUserId === myUserId) return promote.a
+		if (myUserId && b.authorUserId === myUserId) return promote.b
 
 		if (a.likes > b.likes) return promote.a
 		if (a.likes < b.likes) return promote.b
