d97b17f4e0be9920dfbbac3559e415b15b4f3c23
Chase Moskal <chasemoskal@gmail.com>
2021-06-29 22:56:34 -0700

    debug: add logging to mailgun

--- s/assembly/backend/configurators/configure-mailgun.ts
+++ s/assembly/backend/configurators/configure-mailgun.ts
@@ -1,17 +1,20 @@
 
 import mailgun from "mailgun-js"
 import {ConfigEmailMailgun} from "../types/config-email-mailgun.js"
+import {sendEmail as mockSendEmail} from "../../../features/auth/tools/emails/mock-send-email.js"
 
 export function configureMailgun({mailgun: {apiKey, from, sendingDomain}}: ConfigEmailMailgun) {
 	const mg = mailgun({apiKey, domain: sendingDomain})
 	return {
 		sendEmail: async({to, body, subject}) => {
+			await mockSendEmail({to, body, subject})
 			await mg.messages().send({
 				from,
 				to,
 				subject,
 				text: body,
 			})
+			console.log("sent via mailgun")
 		},
 	}
 }
