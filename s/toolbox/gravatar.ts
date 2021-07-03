77d4a1cd911a6ee0d9cb49b7c85a80ab3ff2f6ca
Chase Moskal <chasemoskal@gmail.com>
2021-07-03 05:25:02 -0700

    remove: old dependencies

--- s/toolbox/gravatar.ts
+++ s/toolbox/gravatar.ts
@@ -1,38 +1,38 @@
 
-import {MD5} from "crypto-es/lib/md5.js"
- //               ^^
- // Could not find a declaration file for module
+// import {MD5} from "crypto-es/lib/md5.js"
+//  //               ^^
+//  // Could not find a declaration file for module
 
-export function gravatar({email, ...options}: {
-		email: string
-		size?: number
-		default?:
-			| "404"
-			| "mp"
-			| "identicon"
-			| "monsterid"
-			| "wavatar"
-			| "retro"
-			| "robohash"
-			| "blank"
-			| string
-	}) {
+// export function gravatar({email, ...options}: {
+// 		email: string
+// 		size?: number
+// 		default?:
+// 			| "404"
+// 			| "mp"
+// 			| "identicon"
+// 			| "monsterid"
+// 			| "wavatar"
+// 			| "retro"
+// 			| "robohash"
+// 			| "blank"
+// 			| string
+// 	}) {
 
-	const query = makeQueryString({
-		d: options.default,
-		s: options.size
-			? options.size.toString()
-			: undefined,
-	})
+// 	const query = makeQueryString({
+// 		d: options.default,
+// 		s: options.size
+// 			? options.size.toString()
+// 			: undefined,
+// 	})
 
-	return `https://www.gravatar.com/avatar/${MD5(email)}${query}`
-}
+// 	return `https://www.gravatar.com/avatar/${MD5(email)}${query}`
+// }
 
-function makeQueryString(params: {[key: string]: string | undefined}) {
-	const entries = Object.entries(params)
-	return entries.length
-		? "?" + entries
-			.map(([key, value]) => `${key}=${value}`)
-			.join("&")
-		: ""
-}
+// function makeQueryString(params: {[key: string]: string | undefined}) {
+// 	const entries = Object.entries(params)
+// 	return entries.length
+// 		? "?" + entries
+// 			.map(([key, value]) => `${key}=${value}`)
+// 			.join("&")
+// 		: ""
+// }
