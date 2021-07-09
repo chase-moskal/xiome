408b8258e839e6b31fbf1d3134086e55361cfd84
Chase Moskal <chasemoskal@gmail.com>
2021-07-09 02:18:36 -0700

    damnedb: shelf project to wait for mongo@4

--- s/toolbox/damnedb/damnedb.ts
+++ s/toolbox/damnedb/damnedb.ts
@@ -1,55 +1,66 @@
 
-import {DamnedId} from "./damned-id.js"
-import {MongoClient, ClientSession} from "mongodb"
+// //
+// // WHAT HAPENED TO DAMNEDB?
+// //
+// // this is a sketch of a future dbby replacment
+// // designed for mongodb session-level transactions
+// // and perhaps adding postgres support
+// //
+// // we shelved this project,
+// // because we need to wait for mongodb@4
+// //
 
-export type DamnedValue =
-	| DamnedId
-	| undefined
-	| boolean
-	| number
-	| string
-	| bigint
+// import {DamnedId} from "./damned-id.js"
+// import {MongoClient, ClientSession} from "mongodb"
 
-export type DamnedRow = {[key: string]: DamnedValue}
-export type DamnedSchema = {}
-export type Schema = {}
-export type Tables = any
-export type SchemaToTables<xSchema extends Schema> = Tables
-export type Action<xTables extends Tables, xResult>= (tables: xTables) => Promise<xResult>
+// export type DamnedValue =
+// 	| DamnedId
+// 	| undefined
+// 	| boolean
+// 	| number
+// 	| string
+// 	| bigint
 
-export type Damnedb<xTables extends Tables> = {
-	tables: xTables
-	transaction<xResult>(
-			action: Action<xTables, xResult>
-		): Promise<xResult>
-}
+// export type DamnedRow = {[key: string]: DamnedValue}
+// export type DamnedSchema = {}
+// export type Schema = {}
+// export type Tables = any
+// export type SchemaToTables<xSchema extends Schema> = Tables
+// export type Action<xTables extends Tables, xResult>= (tables: xTables) => Promise<xResult>
 
-export function damnedb<xSchema extends Schema>(
-		schema: xSchema,
-		client: MongoClient
-	): Damnedb<SchemaToTables<xSchema>> {
+// export type Damnedb<xTables extends Tables> = {
+// 	tables: xTables
+// 	transaction<xResult>(
+// 			action: Action<xTables, xResult>
+// 		): Promise<xResult>
+// }
 
-	function getTables(session: ClientSession): Tables {}
+// export function damnedb<xSchema extends Schema>(
+// 		schema: xSchema,
+// 		client: MongoClient
+// 	): Damnedb<SchemaToTables<xSchema>> {
 
-	return {
-		async transaction(action) {
-			const session = client.startSession()
-			const collections = getTables(session)
-			try {
-				const result = await action(collections)
-				session.commitTransaction()
-				return result
-			}
-			catch (error) {
-				session.abortTransaction()
-				throw error
-			}
-		},
-		tables: {},
-	}
-}
+// 	function getTables(session: ClientSession): Tables {}
 
-async function lol() {
-	const client = new MongoClient("lol")
-	const database = damnedb({}, client)
-}
+// 	return {
+// 		async transaction(action) {
+// 			const session = client.startSession()
+// 			const collections = getTables(session)
+// 			try {
+// 				const result = await action(collections)
+// 				session.commitTransaction()
+// 				return result
+// 			}
+// 			catch (error) {
+// 				session.abortTransaction()
+// 				throw error
+// 			}
+// 		},
+// 		tables: {},
+// 	}
+// }
+
+// async function lol() {
+// 	const client = new MongoClient("lol")
+// 	const database = damnedb({}, client)
+// }
