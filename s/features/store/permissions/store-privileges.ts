78a1aa6272062fee9cd566358134d9b103d25307
Chase Moskal <chasemoskal@gmail.com>
2021-07-21 15:16:25 -0700

    store: comment out vestige permissions

--- s/features/store/permissions/store-privileges.ts
+++ s/features/store/permissions/store-privileges.ts
@@ -1,5 +1,5 @@
 
-export const storePrivileges = {
-	"manage store": "ced13b93102a9a0af75a1e68705921077ef167fe2408a0deaed88672037ce87f",
-	"give away freebies": "1017ca2d1a809775af2b703d30c836fdbd8aa345bff715a0f9332d2b13486164",
-}
+// export const storePrivileges = {
+// 	"manage store": "ced13b93102a9a0af75a1e68705921077ef167fe2408a0deaed88672037ce87f",
+// 	"give away freebies": "1017ca2d1a809775af2b703d30c836fdbd8aa345bff715a0f9332d2b13486164",
+// }
