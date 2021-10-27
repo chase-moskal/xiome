90a8b2dc997f04c4fb879cc24b53066f98b4be02
Chase Moskal <chasemoskal@gmail.com>
2021-10-27 09:47:43 -0700

    notes: tweak whitespace

--- s/features/notes/api/validation/validate-pagination.ts
+++ s/features/notes/api/validation/validate-pagination.ts
@@ -1,5 +1,6 @@
-import {max, min, number, schema, Validator, validator} from "../../../../toolbox/darkvalley.js"
+
 import {Pagination} from "../../types/notes-concepts.js"
+import {max, min, number, schema, validator} from "../../../../toolbox/darkvalley.js"
 
 const maxNotesPerPage = 100
 
