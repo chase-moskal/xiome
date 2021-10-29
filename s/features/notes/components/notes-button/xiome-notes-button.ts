16ffb6ab43d8f0dcd399893b70f6ac30726b659e
zenkyuv <przemekgg2002@gmail.com>
2021-10-29 19:25:14 +0200

    added: bell icon with indicator

--- s/features/notes/components/notes-button/xiome-notes-button.ts
+++ s/features/notes/components/notes-button/xiome-notes-button.ts
@@ -2,25 +2,28 @@
 import {ops} from "../../../../framework/ops.js"
 import {makeNotesModel} from "../../models/notes-model.js"
 import {ComponentWithShare, mixinStyles, html} from "../../../../framework/component.js"
+import bell from "../../../../framework/icons/bell.svg.js"
+import {property} from "@lit/reactive-element/decorators/property.js"
 
 import styles from "./xiome-notes-button.css.js"
 
 @mixinStyles(styles)
-export class XiomeNotesButton extends ComponentWithShare<{
+export class XiomeNotesIndicator extends ComponentWithShare<{
 		notesModel: ReturnType<typeof makeNotesModel>
 	}> {
-
+	@property({attribute: 'no-icon' })
+	noIcon: boolean = true;
+ 
 	render() {
+		
 		const {state} = this.share.notesModel
 		const stats = ops.value(state.statsOp) ?? {
 			newCount: 0,
 			oldCount: 0,
 		}
 		return html`
-			<button>
-				<span>${stats.newCount}</span>
-				<span>notes icon goes here</span>
-			</button>
+				<span class="count">${stats.newCount}</span>
+				<span class="bell">${bell}</span>
 		`
 	}
 }
