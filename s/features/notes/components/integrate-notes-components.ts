20f6fdcff516ed62afe966a17845bfe23765481b
zenkyuv <przemekgg2002@gmail.com>
2021-10-29 19:25:14 +0200

    added: bell icon with indicator

--- s/features/notes/components/integrate-notes-components.ts
+++ s/features/notes/components/integrate-notes-components.ts
@@ -1,6 +1,6 @@
 
 import {XiomeNotes} from "./notes/xiome-notes.js"
-import {XiomeNotesButton} from "./notes-button/xiome-notes-button.js"
+import {XiomeNotesIndicator} from "./notes-button/xiome-notes-button.js"
 import {mixinShare, mixinSnapstateSubscriptions} from "../../../framework/component.js"
 import {XiomeComponentOptions} from "../../../assembly/frontend/components/types/xiome-component-options.js"
 
@@ -16,11 +16,11 @@ export function integrateNotesComponents({
 				})(XiomeNotes)
 			)
 		),
-		XiomeNotesButton: (
+		XiomeNotesIndicator: (
 			mixinSnapstateSubscriptions(notesModel.stateSubscribe)(
 				mixinShare({
 					notesModel,
-				})(XiomeNotesButton)
+				})(XiomeNotesIndicator)
 			)
 		),
 	}
--- s/features/notes/components/notes-button/xiome-notes-button.css.ts
+++ s/features/notes/components/notes-button/xiome-notes-button.css.ts
@@ -4,6 +4,37 @@ export default css`
 
 :host {
 	display: inline-block;
+	--xiome-notes-indicator-size: 1em;
+	--xiome-notes-indicator-color: red;
+	--xiome-notes-indicator-bleedout: -0.4em;
+	width: 3em;
+	height: 3em;
+	position: relative;
+}
+:host([no-icon]) .bell {
+	display: none;
 }
 
+:host([no-icon]) .count {
+	right: var(--xiome-notes-indicator-bleedout);
+	top: var(--xiome-notes-indicator-bleedout);
+}
+
+xiome-notes-indicator {
+	width: var(--xiome-notes-indicator-size)
+}
+
+.count {
+	vertical-align: top;
+	position: absolute;
+	height: var(--xiome-notes-indicator-size);
+  width: var(--xiome-notes-indicator-size);
+  background-color: var(--xiome-notes-indicator-color);
+  border-radius: 50%;
+  display: inline-block;
+	text-align: center;
+	right: 5px;
+	font-weight: bold;
+	color: white;
+}
 `
