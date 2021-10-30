17700ce4cd31d0fc4e9d403fe226ae9f52b0060c
zenkyuv <przemekgg2002@gmail.com>
2021-10-30 02:11:21 +0200

    fixed: xiome-notes-indicator

--- s/features/notes/components/notes-indicator/xiome-notes-indicator.css.ts
+++ s/features/notes/components/notes-indicator/xiome-notes-indicator.css.ts
@@ -6,34 +6,31 @@ export default css`
 	display: inline-block;
 	--xiome-notes-indicator-size: 1em;
 	--xiome-notes-indicator-color: red;
-	--xiome-notes-indicator-bleedout: -0.4em;
+	--xiome-notes-indicator-bleedout: 0em;
 	width: 3em;
 	height: 3em;
 	position: relative;
 }
+
 :host([no-icon]) .bell {
 	display: none;
 }
 
-:host([no-icon]) .count {
-	right: var(--xiome-notes-indicator-bleedout);
-	top: var(--xiome-notes-indicator-bleedout);
-}
-
 xiome-notes-indicator {
-	width: var(--xiome-notes-indicator-size)
+	width: var(--xiome-notes-indicator-size);
 }
 
 .count {
 	vertical-align: top;
 	position: absolute;
 	height: var(--xiome-notes-indicator-size);
-  width: var(--xiome-notes-indicator-size);
-  background-color: var(--xiome-notes-indicator-color);
-  border-radius: 50%;
-  display: inline-block;
+	width: var(--xiome-notes-indicator-size);
+	background-color: var(--xiome-notes-indicator-color);
+	border-radius: 50%;
+	display: inline-block;
 	text-align: center;
-	right: 5px;
+	right: var(--xiome-notes-indicator-bleedout: 0em);
+	top: var(--xiome-notes-indicator-bleedout: 0em);
 	font-weight: bold;
 	color: white;
 }
