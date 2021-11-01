0d0caa155d96b46fec158b74a41b8ff198183eea
Chase Moskal <chasemoskal@gmail.com>
2021-11-01 13:57:55 -0700

    uncomment: tests until we found one failing

--- s/features/notes/notes.test.ts
+++ s/features/notes/notes.test.ts
@@ -27,65 +27,65 @@ export default <Suite>{
 		assertNoteCounts({newCount: 2, oldCount: 0})
 	},
 
-	// async "a message for user is readable by user"() {
-	// 	const {userId, backend, frontend} = await notesTestingSetup()
-	// 	const {notesModel} = frontend
-	// 	const {notesDepositBox} = backend
-	// 	const {loadNewPage} = prepareNoteInboxAssertions({notesModel})
+	async "a message for user is readable by user"() {
+		const {userId, backend, frontend} = await notesTestingSetup()
+		const {notesModel} = frontend
+		const {notesDepositBox} = backend
+		const {loadNewPage} = prepareNoteInboxAssertions({notesModel})
 
-	// 	const draft = fakeNoteDraft(userId)
-	// 	await notesDepositBox.sendNote(draft)
+		const draft = fakeNoteDraft(userId)
+		await notesDepositBox.sendNote(draft)
 
-	// 	const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
-	// 	pageTesting.assertNotesLength(1)
-	// 	pageTesting.assertNoteTitle(0, draft.title)
-	// },
+		const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
+		pageTesting.assertNotesLength(1)
+		pageTesting.assertNoteTitle(0, draft.title)
+	},
 
-	// async "a message cannot be read by the wrong user"() {
-	// 	const {rando, backend, frontend} = await notesTestingSetup()
-	// 	const {notesModel} = frontend
-	// 	const {notesDepositBox} = backend
-	// 	const {loadNewPage} = prepareNoteInboxAssertions({notesModel})
+	async "a message cannot be read by the wrong user"() {
+		const {rando, backend, frontend} = await notesTestingSetup()
+		const {notesModel} = frontend
+		const {notesDepositBox} = backend
+		const {loadNewPage} = prepareNoteInboxAssertions({notesModel})
 
-	// 	const draft = fakeNoteDraft(rando.randomId().toString())
-	// 	await notesDepositBox.sendNote(draft)
+		const draft = fakeNoteDraft(rando.randomId().toString())
+		await notesDepositBox.sendNote(draft)
 
-	// 	const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
-	// 	pageTesting.assertNotesLength(0)
-	// },
+		const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
+		pageTesting.assertNotesLength(0)
+	},
 
-	// async "user can mark a message old and then new again"() {
-	// 	const {userId, backend, frontend} = await notesTestingSetup()
-	// 	const {notesModel} = frontend
-	// 	const {notesDepositBox} = backend
-	// 	const {loadNewPage, loadOldPage} = prepareNoteInboxAssertions({notesModel})
+	async "user can mark a message old and then new again"() {
+		const {userId, backend, frontend} = await notesTestingSetup()
+		const {notesModel} = frontend
+		const {notesDepositBox} = backend
+		const {loadNewPage, loadOldPage} = prepareNoteInboxAssertions({notesModel})
 
-	// 	const draft = fakeNoteDraft(userId)
-	// 	await notesDepositBox.sendNote(draft)
+		const draft = fakeNoteDraft(userId)
+		await notesDepositBox.sendNote(draft)
 
-	// 	{
-	// 		const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
-	// 		pageTesting.assertNotesLength(1)
-	// 		const {noteId} = pageTesting.notes[0]
-	// 		await notesModel.markNotesNewOrOld(true, [noteId])
-	// 	}
-	// 	{
-	// 		const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
-	// 		pageTesting.assertNotesLength(0)
-	// 	}
-	// 	{
-	// 		const pageTesting = await loadOldPage({pageSize: 10, pageNumber: 0})
-	// 		pageTesting.assertNotesLength(1)
-	// 		const {noteId} = pageTesting.notes[0]
-	// 		await notesModel.markNotesNewOrOld(false, [noteId])
-	// 	}
-	// 	{
-	// 		const newTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
-	// 		const oldTesting = await loadOldPage({pageSize: 10, pageNumber: 0})
-	// 		oldTesting.assertNotesLength(0)
-	// 		newTesting.assertNotesLength(1)
-	// 	}
-	// },
+		{
+			const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
+			pageTesting.assertNotesLength(1)
+			const {noteId} = pageTesting.notes[0]
+			await notesModel.markNotesNewOrOld(true, [noteId])
+		}
+		{
+			const pageTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
+			pageTesting.assertNotesLength(0)
+		}
+		{
+			const pageTesting = await loadOldPage({pageSize: 10, pageNumber: 0})
+			pageTesting.assertNotesLength(1)
+			const {noteId} = pageTesting.notes[0]
+			await notesModel.markNotesNewOrOld(false, [noteId])
+		}
+		{
+			const newTesting = await loadNewPage({pageSize: 10, pageNumber: 0})
+			const oldTesting = await loadOldPage({pageSize: 10, pageNumber: 0})
+			oldTesting.assertNotesLength(0)
+			newTesting.assertNotesLength(1)
+		}
+	},
 
 	// async "users can manage notes in bulk"() {
 	// 	const {userId, backend, frontend} = await notesTestingSetup()
