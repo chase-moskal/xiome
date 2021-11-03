75b5be9c84159208eeb9c1db638b46c5286d2615
Chase Moskal <chasemoskal@gmail.com>
2021-11-03 13:34:19 -0700

    sketch: skeleton wiring cache to notes component

--- s/features/notes/components/notes/xiome-notes.ts
+++ s/features/notes/components/notes/xiome-notes.ts
@@ -2,16 +2,37 @@
 import styles from "./xiome-notes.css.js"
 import {makeNotesModel} from "../../models/notes-model.js"
 import {ComponentWithShare, mixinStyles, html} from "../../../../framework/component.js"
+import {renderOp} from "../../../../framework/op-rendering/render-op.js"
 
 @mixinStyles(styles)
 export class XiomeNotes extends ComponentWithShare<{
 		notesModel: ReturnType<typeof makeNotesModel>
 	}> {
 
+	#cache = this.share.notesModel.createNotesCache()
+
+	subscribe() {
+		const unsubs = [
+			super.subscribe(),
+			this.#cache.subscribe(() => this.requestUpdate()),
+		]
+		return () => unsubs.forEach(unsub => unsub())
+	}
+
 	render() {
-		const {notesModel} = this.share
+		const {cacheState} = this.#cache
 		return html`
 			<p>xiome-notes</p>
+			${renderOp(cacheState.newNotesOp, newNotes => html`
+				<ol>
+					${newNotes.map(note => html`
+						<li>
+							<p>noteId: ${note.noteId}</p>
+							<p>noteTitle: ${note.title}</p>
+						</li>
+					`)}
+				</ol>
+			`)}
 		`
 	}
 }
