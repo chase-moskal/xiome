0557571e1c86eb0df2aa0ff757fa903bad950c41
Chase Moskal <chasemoskal@gmail.com>
2021-11-06 11:59:49 -0700

    add: better online/offline test

--- s/features/chat/chat.test.ts
+++ s/features/chat/chat.test.ts
@@ -1,42 +1,48 @@
 
-import {assert, Suite} from "cynic"
-import {nap} from "../../toolbox/nap.js"
+import {assert, expect, Suite} from "cynic"
 import {ChatStatus} from "./common/types/chat-concepts.js"
 import {testChatSetup} from "./testing/test-chat-setup.js"
 
 export default<Suite>{
-
-	async "viewer can find that a room starts offline"() {
-		const setup = await testChatSetup()
-		const server = await setup.makeServer()
-		const {chatModel} = await server.makeClientFor.viewer()
-		const room = await chatModel.room("default")
-		assert(room.status === ChatStatus.Offline, "room should start offline")
+	"room online/offline behavior": {
+		async "viewer can find that a room starts offline"() {
+			const setup = await testChatSetup()
+			const server = await setup.makeServer()
+			const {chatModel} = await server.makeClientFor.viewer()
+			const room = await chatModel.room("default")
+			assert(room.status === ChatStatus.Offline, "room should start offline")
+		},
+	
+		async "moderator can and set a room online"() {
+			const setup = await testChatSetup()
+			const server = await setup.makeServer()
+			const {chatModel} = await server.makeClientFor.moderator()
+			const room = await chatModel.room("default")
+			assert(room.status === ChatStatus.Offline, "room should start offline")
+			room.setRoomStatus(ChatStatus.Online)
+			assert(room.status === ChatStatus.Online, "room should be online")
+		},
+	
+		async "moderator can set a room online, other participant sees this"() {
+			const setup = await testChatSetup()
+			const server = await setup.makeServer()
+	
+			// participant sees room offline
+			const participant = await server.makeClientFor.participant()
+			const participantRoom = await participant.chatModel.room("default")
+			expect(participantRoom.status).equals(ChatStatus.Offline)
+	
+			// moderator sets room online
+			const moderator = await server.makeClientFor.moderator()
+			const moderatorRoom = await moderator.chatModel.room("default")
+			expect(moderatorRoom.status).equals(ChatStatus.Offline)
+			moderatorRoom.setRoomStatus(ChatStatus.Online)
+			expect(moderatorRoom.status).equals(ChatStatus.Online)
+	
+			// participant sees it
+			expect(participantRoom.status).equals(ChatStatus.Online)
+			expect((await participant.chatModel.room("default")).status)
+				.equals(ChatStatus.Online)
+		},
 	},
-
-	async "moderator can and set a room online"() {
-		const setup = await testChatSetup()
-		const server = await setup.makeServer()
-		const {chatModel} = await server.makeClientFor.moderator()
-		const room = await chatModel.room("default")
-		assert(room.status === ChatStatus.Offline, "room should start offline")
-		room.setRoomStatus(ChatStatus.Online)
-		assert(room.status === ChatStatus.Online, "room should be online")
-	},
-
-	// async "participant can receive their own message"() {
-	// 	const setup = await testChatSetup()
-	// 	const server = await setup.makeServer()
-	// 	{
-	// 		const {chatModel} = await server.makeClientFor.moderator()
-	// 		const room = await chatModel.room("default")
-	// 		room.setRoomStatus(ChatStatus.Online)
-	// 		await room.waitMessages(1)
-	// 	}
-	// 	{
-	// 		const {chatModel} = await server.makeClientFor.participant()
-	// 		const room = await chatModel.room("default")
-	// 		assert(room.status === ChatStatus.Online, "room should be online")
-	// 	}
-	// },
 }
