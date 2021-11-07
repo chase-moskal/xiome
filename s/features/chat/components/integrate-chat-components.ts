adef86d4dca0801342a81b7f29d4d63cebb17793
Chase Moskal <chasemoskal@gmail.com>
2021-11-07 11:41:35 -0800

    integrate: xiome-chat component

--- s/features/chat/components/integrate-chat-components.ts
+++ s/features/chat/components/integrate-chat-components.ts
@@ -3,7 +3,7 @@ import {XiomeChat} from "./xiome-chat/xiome-chat.js"
 import {mixinSnapstateSubscriptions, mixinShare} from "../../../framework/component.js"
 import {XiomeComponentOptions} from "../../../assembly/frontend/components/types/xiome-component-options.js"
 
-export function integrateExampleComponents({models, modals}: XiomeComponentOptions) {
+export function integrateChatComponents({models, modals}: XiomeComponentOptions) {
 	const {chatModel} = models
 	return {
 		XiomeChat: (
