7d98bea8ac731b8cec602ea7567a671d1dc45f1d
Chase Moskal <chasemoskal@gmail.com>
2021-11-10 11:27:32 -0800

    add: subbies interface

--- s/toolbox/subbies.ts
+++ s/toolbox/subbies.ts
@@ -1,15 +1,23 @@
 
 type Listener<Context> = (context: Context) => void
 
-export function subbies<Context = undefined>() {
+export interface Subbie<Context = undefined> {
+	subscribe(listener: Listener<Context>): () => void
+	publish: Listener<Context>
+	clear(): void
+}
+
+export function subbies<Context = undefined>(): Subbie<Context> {
 	const memory = new Map<symbol, Listener<Context>>()
 	return {
 		subscribe(listener: Listener<Context>) {
 			const symbol = Symbol()
 			memory.set(symbol, listener)
-			return () => memory.delete(symbol)
+			return () => {
+				memory.delete(symbol)
+			}
 		},
-		publish: <Listener<Context>>(context => {
+		publish: (context => {
 			for (const [,listener] of memory)
 				listener(context)
 		}),
