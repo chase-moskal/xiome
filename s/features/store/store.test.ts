8a2839ddc177d3c1998623f6ddd04dc22dcf9c10
Chase Moskal <chasemoskal@gmail.com>
2021-11-14 13:49:32 -0800

    sketch: new store testing ideas

---
+++ s/features/store/store.test.ts
@@ -0,0 +1,36 @@
+
+import {Suite} from "cynic"
+
+export default <Suite>{
+	async "shopkeeping"() {
+		return {
+			async "merchant can link a bank account"() {},
+			async "merchant can flip store status online/offline"() {},
+		}
+	},
+	async "subscriptions"() {
+		return {
+			"planning": {
+				async "merchant can create a subscription plan"() {},
+				async "merchant can deactivate/activate subscription plans"() {},
+			},
+			"sales": {
+				async "customer can purchase a subscription"() {},
+				async "customer can cancel a subscription"() {},
+				async "customer can update subscription's payment method"() {},
+				async "subscription ends if automatic renewal fails"() {},
+			},
+			"bookkeeping": {
+				async "customer can view a subscription's details"() {},
+				async "customer can view payment history"() {},
+			},
+		}
+	},
+	async "digital products"() {
+		return {
+			async "merchant can manage digital product listings"() {},
+			async "customer can purchase a digital product"() {},
+			async "customer can obtain product ownership tokens"() {},
+		}
+	},
+}
