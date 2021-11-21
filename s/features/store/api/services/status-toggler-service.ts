6daeda69e108e83eb78e605a91313bd1a75e6ae1
Chase Moskal <chasemoskal@gmail.com>
2021-11-20 17:09:45 -0800

    cleanup: import in store service

--- s/features/store/api/services/status-toggler-service.ts
+++ s/features/store/api/services/status-toggler-service.ts
@@ -3,7 +3,6 @@ import {apiContext} from "renraku/x/api/api-context.js"
 
 import {StoreServiceOptions} from "../types/store-options.js"
 import {StoreAuth, StoreMeta} from "../types/store-metas-and-auths.js"
-import {appPermissions} from "../../../../assembly/backend/permissions/standard-permissions.js"
 
 export const makeStatusTogglerService = (
 		options: StoreServiceOptions
