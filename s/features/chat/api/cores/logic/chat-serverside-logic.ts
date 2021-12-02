02cf7c4d2790725e9d83038fe30f075022fdae72
Przemek Gałęzki <przemekgg2002@gmail.com>
2021-12-02 13:50:35 +0100

    add: chat serverside validation

--- s/features/chat/api/cores/logic/chat-serverside-logic.ts
+++ s/features/chat/api/cores/logic/chat-serverside-logic.ts
@@ -1,6 +1,6 @@
 
 import {Rando} from "../../../../../toolbox/get-rando.js"
-import {validateRoom} from "../../../common/chat-validators.js"
+import {validateDraft, validatePostIds, validateRoom, validateStatus, validateUserIds} from "../../../common/chat-validators.js"
 import {chatAllowance} from "../../../common/chat-allowance.js"
 import {ChatDraft, ChatMeta, ChatPersistence, ChatPolicy, ChatPost, ChatStatus, ClientRecord} from "../../../common/types/chat-concepts.js"
 
@@ -42,11 +42,13 @@ export function prepareChatServersideLogic({
 			clientRemote.roomStatusChanged(room, await persistence.getRoomStatus(room))
 		},
 		async roomUnsubscribe(room: string) {
+			enforceValidation(validateRoom(room))
 			if (!getAllowance().viewAllChats)
 				return undefined
 			clientRecord.rooms.delete(room)
 		},
 		async post(room: string, draft: ChatDraft) {
+			enforceValidation([...validateDraft(draft), ...validateRoom(room)])
 			if (!getAllowance().participateInAllChats)
 				return undefined
 			if (isNotMuted() && isNotBanned()) {
@@ -62,21 +64,25 @@ export function prepareChatServersideLogic({
 			}
 		},
 		async remove(room: string, postIds: string[]) {
+			enforceValidation([...validateRoom(room), ...validatePostIds(postIds)])
 			if (!getAllowance().moderateAllChats)
 				return undefined
 			await persistence.removePosts(room, postIds)
 		},
 		async clear(room: string) {
+			enforceValidation(validateRoom(room))
 			if (!getAllowance().moderateAllChats)
 				return undefined
 			await persistence.clearRoom(room)
 		},
 		async mute(userIds: string[]) {
+			enforceValidation(validateUserIds(userIds))
 			if (!getAllowance().moderateAllChats)
 				return undefined
 			await persistence.addMute(userIds)
 		},
 		async unmute(userIds: string[]) {
+			enforceValidation(validateUserIds(userIds))
 			if (!getAllowance().moderateAllChats)
 				return undefined
 			await persistence.removeMute(userIds)
@@ -87,6 +93,7 @@ export function prepareChatServersideLogic({
 			await persistence.unmuteAll()
 		},
 		async setRoomStatus(room: string, status: ChatStatus) {
+			enforceValidation([...validateRoom(room), ...validateStatus(status)])
 			if (!getAllowance().moderateAllChats)
 				return undefined
 			await persistence.setRoomStatus(room, status)
