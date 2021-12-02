01985fe956819da7059ab2e6bf9aabc8c673b9ec
Przemek Gałęzki <przemekgg2002@gmail.com>
2021-12-02 13:50:35 +0100

    add: chat serverside validation

--- s/features/chat/common/chat-validators.ts
+++ s/features/chat/common/chat-validators.ts
@@ -1,9 +1,32 @@
 
-import {maxLength, minLength, string, validator, zeroWhitespace} from "../../../toolbox/darkvalley.js"
+import {validateId} from "../../../common/validators/validate-id.js"
+import {each, max, maxLength, min, minLength, number, schema, string, validator, zeroWhitespace} from "../../../toolbox/darkvalley.js"
 
 export const validateRoom = validator(
 	string(),
 	minLength(1),
 	maxLength(32),
 	zeroWhitespace(),
+)
+
+export const validateDraft = schema<{
+	content: string
+}>({
+	content: validator(
+		string(),
+	)
+})
+
+export const validateUserIds = validator(
+	each(validateId)
+)
+
+export const validatePostIds = validator(
+	each(validateId)
+)
+
+export const validateStatus = validator(
+	number(),
+	min(0),
+	max(1)
 )
