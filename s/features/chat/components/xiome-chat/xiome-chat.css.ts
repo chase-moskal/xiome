9d3c186c82a1c2e25b9f204cb867fe39fe0c2533
gelatinadenaranja <nahueldeleeuw@hotmail.com>
2021-12-16 18:42:45 -0300

    First css rules for the chat frame style

--- s/features/chat/components/xiome-chat/xiome-chat.css.ts
+++ s/features/chat/components/xiome-chat/xiome-chat.css.ts
@@ -7,6 +7,38 @@ X * { outline: 1px solid #f902; }
 
 :host {
 	display: block;
+    --pad: var(--xio-chat-pad, 0);
+    --background: var(--xio-chat-background, transparent);
+    --border: var(--xio-chat-border, none);
+    --border-radius: var(--xio-chat-border-radius, 0em);
+}
+
+.chatbox {
+	padding var(--pad);
+	background: var(--background);
+	border: var(--border);
+	border-radius: var(--border-radius);
+}
+
+.chatbox header {
+	display: block;
+}
+
+.chatbox header > span {
+	display: inline-flex;
+	justify-content: center;
+	align-content: center;
+	--xio-button-padding: 0.3em;
+}
+
+div .authorship {
+	display: flex;
+	align-items: center;
+}
+
+div[slot="logged-out"] {
+	text-align: center;
+	line-height: 2em;
 }
 
 ${chatHistoryCss}
