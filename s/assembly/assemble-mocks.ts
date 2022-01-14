40b46999e8b500a721779be8a44e48c5f8b6283d
Chase Moskal <chasemoskal@gmail.com>
2022-01-13 23:18:15 -0800

    cleanup: dead imports

--- s/assembly/assemble-mocks.ts
+++ s/assembly/assemble-mocks.ts
@@ -3,9 +3,7 @@ import {Await} from "../types/await.js"
 import {mockConnect} from "./frontend/connect/mock/mock-connect.js"
 import {applyMockHacks} from "./frontend/mocks/apply-mock-hacks.js"
 import {XiomeMockConfig} from "./frontend/types/xiome-config-mock.js"
-import {chatSocketClient} from "../features/chat/api/sockets/chat-socket-client.js"
 import {assembleAndInitializeFrontend} from "./frontend/assemble-and-initialize-frontend.js"
-import {chatMockClientEntirely} from "../features/chat/api/sockets/chat-mock-client-entirely.js"
 
 export async function assembleXiomeMock(mockConfig: XiomeMockConfig) {
 	const connection = await mockConnect(mockConfig)
