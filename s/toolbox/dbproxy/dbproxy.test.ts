0bc9bdb91460f6ac0bd8bc2468d02c261275d59c
Chase Moskal <chasemoskal@gmail.com>
2022-01-20 08:13:02 -0800

    add: fallback test

--- s/toolbox/dbproxy/dbproxy.test.ts
+++ s/toolbox/dbproxy/dbproxy.test.ts
@@ -2,8 +2,11 @@
 import {Suite, expect} from "cynic"
 
 import id from "./id.test.js"
+
+import {find} from "./helpers.js"
+import {fallback} from "./fallback.js"
 import * as dbproxy from "./dbproxy.js"
-import {memoryFlexStorage} from "../flex-storage/memory-flex-storage.js"
+import {SchemaToShape} from "./types.js"
 
 type DemoUser = {
 	userId: string
@@ -11,12 +14,16 @@ type DemoUser = {
 	location: string
 }
 
+type DemoSchema = {
+	users: DemoUser
+}
+
+const demoShape: SchemaToShape<DemoSchema> = {
+	users: true
+}
+
 async function setupThreeUserDemo() {
-	type Database = {users: {userId: string, balance: number, location: string}}
-	const {tables} = dbproxy.flexDatabase<Database>(
-		memoryFlexStorage(),
-		{users: true}
-	)
+	const {tables} = dbproxy.memoryDatabase<DemoSchema>(demoShape)
 	await Promise.all([
 		tables.users.create({userId: "u123", balance: 100, location: "america"}),
 		tables.users.create({userId: "u124", balance: 0, location: "canada"}),
@@ -27,6 +34,19 @@ async function setupThreeUserDemo() {
 
 export default <Suite>{
 	id,
+	"fallback": {
+		"read": async() => {
+			const {users} = await setupThreeUserDemo()
+			const {tables: {users: usersFallback}}
+				= dbproxy.memoryDatabase<DemoSchema>(demoShape)
+			await usersFallback.create({userId: "u92", balance: 92, location: "victoria"})
+			const combinedTable = fallback({table: users, fallbackTable: usersFallback})
+			const result01 = await combinedTable.read({conditions: false})
+			const result02 = await combinedTable.read(find({userId: "u92"}))
+			expect(result01.length).equals(4)
+			expect(result02.length).equals(1)
+		},
+	},
 	"flex database": {
 		"create rows and read 'em back unconditionally": async() => {
 			const tables = await setupThreeUserDemo()
--- s/toolbox/dbproxy/dbproxy.ts
+++ s/toolbox/dbproxy/dbproxy.ts
@@ -2,3 +2,4 @@
 export * from "./id.js"
 export * from "./types.js"
 export * from "./flex-database.js"
+export * from "./memory-database.js"
