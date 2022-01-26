82efde46dec87e2abfa5f6c65e27aa6d38082d8c
Chase Moskal <chasemoskal@gmail.com>
2022-01-26 12:46:15 -0800

    replace: flex proxy with plain object, fix async
     - the flex proxy caused strange issues when a proxy table was returned
       by an async function (the `then` property would be queried, as a
       table, which was causing issues)

--- s/toolbox/dbproxy/databases/flex.ts
+++ s/toolbox/dbproxy/databases/flex.ts
@@ -4,9 +4,8 @@ import {objectMap} from "../../object-map.js"
 import {RowStorage} from "./flex/row-storage.js"
 import {sequencer} from "../../sequencer/sequencer.js"
 import {memoryTransaction} from "./flex/memory-transaction.js"
-import {pathToStorageKey} from "./utils/path-to-storage-key.js"
-import {Database, Schema, SchemaToShape, Shape} from "../types.js"
 import {FlexStorage} from "../../flex-storage/types/flex-storage.js"
+import {Database, Row, Schema, SchemaToShape, Shape, Table} from "../types.js"
 
 export function flex<xSchema extends Schema>(
 		flexStorage: FlexStorage,
@@ -22,29 +21,24 @@ export function flex<xSchema extends Schema>(
 			function recurse(innerShape: Shape, path: string[]) {
 				return objectMap(innerShape, (value, key) => {
 					const currentPath = [...path, key]
-					return typeof value === "boolean"
-						// TODO consider replacing proxy with regular object,
-						// because the proxy behaves strangely when returned
-						// from an async function
-						? new Proxy({}, {
-							get(target, prop) {
-								if (!target[prop]) {
-									target[prop] = async(...args: any[]) => safeMemoryTransaction({
-										shape,
-										storage,
-										async action({tables}) {
-											return obtain(tables, currentPath)[prop](...args)
-										},
-									})
-								}
-								return target[prop]
-							},
-							set() {
-								throw new Error(
-									`table "${pathToStorageKey(currentPath)}" is readonly`
-								)
-							},
+					function prep(method: keyof Table<Row>) {
+						return async(...args: any[]) => safeMemoryTransaction({
+							shape,
+							storage,
+							action: async({tables}) => (
+								obtain(tables, currentPath)[method](...args)
+							),
 						})
+					}
+					return typeof value === "boolean"
+						? <Table<Row>>{
+							create: prep("create"),
+							read: prep("read"),
+							update: prep("update"),
+							delete: prep("delete"),
+							count: prep("count"),
+							readOne: prep("readOne"),
+						}
 						: recurse(value, currentPath)
 				})
 			}
