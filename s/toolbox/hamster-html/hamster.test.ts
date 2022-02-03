0e8081ad138ef9ed3b8f330fc402d1bc522d9fff
Paul_Aroo <timmypaul2006@gmail.com>
2022-02-03 02:24:37 +0100

    add: tests for hamster-html

---
+++ s/toolbox/hamster-html/hamster.test.ts
@@ -0,0 +1,28 @@
+
+import { Suite, assert, expect } from "cynic";
+import { html, render } from "./html.js";
+
+export default <Suite>{
+	"santization": {
+		"sanitization occurs by default": async () => {
+			const input = html`${"<div></div>"}`
+			const output = "&lt;div&gt;&lt;/div&gt;"
+
+			assert(render(input)===output,"sanitization does not occur")
+		},
+		"nested html functions must not be sanitized": async () => {
+			const input = html`${html`<div></div>`}`
+			const output = "<div></div>"
+	
+			assert(render(input)===output, "nested html function is sanitized")
+		}
+	},
+	"nesting": {
+		"multiple html functions can be nested": async () => {
+			const input = html`${html`${html`${html`<div></div>`}`}`}`
+			const output = "<div></div>"
+
+			expect(render(input)).equals(output)
+		}
+	}
+}
