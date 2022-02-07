cdf9f98f47fa2b26eb67419826f31032c488f830
Chase Moskal <chasemoskal@gmail.com>
2022-02-07 06:12:03 -0800

    fix: array value processing

--- s/toolbox/hamster-html/html.ts
+++ s/toolbox/hamster-html/html.ts
@@ -4,17 +4,25 @@ import {escapeHtml} from "../escape-html.js"
 export class HtmlTemplate {
 	#strings: string[]
 	#values: any[]
+
 	constructor({strings, values}) {
 		this.#strings = strings
 		this.#values = values
 	}
+
+	#processValue(value: any) {
+		return value instanceof HtmlTemplate
+			? value.toString()
+			: escapeHtml(value.toString())
+	}
+
 	toString() {
 		return this.#strings.reduce(
 			(previous, current, index) => {
 				const value = this.#values[index] ?? ""
-				const safeValue = value instanceof HtmlTemplate
-					? value.toString()
-					: escapeHtml(value.toString())
+				const safeValue = Array.isArray(value)
+					? value.map(this.#processValue).join("")
+					: this.#processValue(value)
 				return previous + current + safeValue
 			}, ""
 		)
