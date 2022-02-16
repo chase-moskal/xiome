3950671a246ded019647fb413d61c71ca334ae59
Chase Moskal <chasemoskal@gmail.com>
2022-02-16 12:56:28 -0800

    add: web2 scss

---
+++ web2/html/partials/page.html.ts
@@ -0,0 +1,11 @@
+
+// import {html} from "../../"
+
+// export default function page({}: {
+// 		mode: string
+// 		base: string
+// 		pageSubtitle: string
+// 		pageName: string
+// 	}) {
+// 	return html``
+// }
