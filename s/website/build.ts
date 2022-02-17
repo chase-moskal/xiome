d0574bb60c3ad23ac150cccccadd8772e2d6bd1a
Chase Moskal <chasemoskal@gmail.com>
2022-02-16 17:39:13 -0800

    fix: automated html base paths

--- s/website/build.ts
+++ s/website/build.ts
@@ -1,12 +1,15 @@
 
-import {makeFileWriter} from "./build/file-writer.js"
-
 import {promisify} from "util"
 import globCallback from "glob"
-import {CommonBuildOptions} from "./build-types.js"
 const glob = promisify(globCallback)
 
+import {dirname, relative} from "path"
+import {CommonBuildOptions} from "./build-types.js"
+import {makeFileWriter} from "./build/file-writer.js"
+import {escapeRegex} from "../toolbox/escape-regex.js"
+
 const root = "x"
+const htmlDirectory = "s/website/html"
 const mode = <CommonBuildOptions["mode"]>process.argv[2]
 const {write} = makeFileWriter(root)
 
@@ -14,20 +17,23 @@ if (!mode)
 	console.error(`build requires argument "${mode}"`)
 
 const htmlSources = await glob(
-	`s/website/html/**/*.html.ts`,
-	{ignore: `s/website/html/partials/**/*`},
+	`${htmlDirectory}/**/*.html.ts`,
+	{ignore: `${htmlDirectory}/partials/**/*`},
 )
 
 const options: CommonBuildOptions = {mode, base: "."}
 
 await Promise.all(htmlSources.map(async source => {
-	source = source
-		.replace(/^s\/website\//, "./")
+	const htmlDirectoryRegex = new RegExp("^" + escapeRegex(htmlDirectory) + "/")
+	const module = source
+		.replace(htmlDirectoryRegex, "./html/")
 		.replace(/\.ts$/, ".js")
-	const renderer = (await import(source)).default
-	const destination = source
+	const renderer = (await import(module)).default
+	const destination = module
 		.replace(/^\.\/html\//, "")
 		.replace(/\.js$/, "")
+	let base = relative(dirname(source), htmlDirectory)
+	base = base === "" ? "." : base
 	console.log(destination)
-	await write(destination, await renderer(options))
+	await write(destination, await renderer({...options, base}))
 }))
