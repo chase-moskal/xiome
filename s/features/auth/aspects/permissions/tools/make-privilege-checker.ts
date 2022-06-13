66d7af69e9771c04b92c895ddfb8e606c4bcc4a4
Chase Moskal <chasemoskal@gmail.com>
2022-06-13 12:20:20 -0700

    update: all dependencies

--- s/features/auth/aspects/permissions/tools/make-privilege-checker.ts
+++ s/features/auth/aspects/permissions/tools/make-privilege-checker.ts
@@ -9,18 +9,24 @@ export function makePrivilegeChecker<xPrivileges extends {[key: string]: string}
 		privileges: xPrivileges,
 	): PrivilegeChecker<xPrivileges> {
 
+	function s(k: any) {
+		return typeof k === "string"
+			? k
+			: String(k)
+	}
+
 	function hasPrivilege(key: keyof xPrivileges) {
 		return permit.privileges.includes(privileges[key])
 	}
 
 	function requirePrivilege(key: keyof xPrivileges) {
 		if (!hasPrivilege(key))
-			throw new renraku.ApiError(403, `forbidden; privilege required "${key}"`)
+			throw new renraku.ApiError(403, `forbidden; privilege required "${s(key)}"`)
 	}
 
 	function requireNotHavePrivilege(key: keyof xPrivileges) {
 		if (hasPrivilege(key))
-			throw new renraku.ApiError(403, `forbidden: must not have privilege "${key}"`)
+			throw new renraku.ApiError(403, `forbidden: must not have privilege "${s(key)}"`)
 	}
 
 	return {
