6011946c038450ce723a04c4fd4bed729a65f14d
Paul_Aroo <timmypaul2006@gmail.com>
2022-06-25 22:54:24 +0100

    wip: handle cancel subscription

--- s/features/store2/api/services/helpers/get-current-stripe-subscription.ts
+++ s/features/store2/api/services/helpers/get-current-stripe-subscription.ts
@@ -1,15 +1,36 @@
+import {find} from "dbmage"
+import Stripe from "stripe"
+import {getStripeId} from "../../../stripe/liaison/helpers/get-stripe-id.js"
 import {StoreCustomerAuth} from "../../types.js"
 
 
 
-export async function getCurrentStripeSubscription(auth: StoreCustomerAuth) {
+export async function getCurrentStripeSubscription(
+		auth: StoreCustomerAuth, tierId: string
+	) {
 
+	// const stripeSubscriptions = await auth.stripeLiaisonAccount
+	// 	.subscriptions.list({customer: auth.stripeCustomerId})
+
+	// if (stripeSubscriptions.data.length > 1)
+	// 	throw new Error("error, more than one subscription")
+
+	// const stripeSubscription = stripeSubscriptions.data ?? []
+	// return stripeSubscription
 	const stripeSubscriptions = await auth.stripeLiaisonAccount
 		.subscriptions.list({customer: auth.stripeCustomerId})
 
-	if (stripeSubscriptions.data.length > 1)
-		throw new Error("error, more than one subscription")
+	let stripeSubscription = undefined as Stripe.Subscription
 
-	const [stripeSubscription] = stripeSubscriptions.data ?? []
+	if(stripeSubscriptions){
+		for (const sub of stripeSubscriptions.data) {
+			const [stripePriceId] = sub
+				.items.data.map(item => getStripeId(item.price))
+			const tierRow = await auth.storeDatabase.tables.subscriptions
+				.tiers.readOne(find({stripePriceId}))
+			if (tierRow.tierId.string === tierId) stripeSubscription = sub
+		}
+	}
+
 	return stripeSubscription
 }
