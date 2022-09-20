31c6ea8e722774b3f69a942a2a51f4fbe7656ae3
Chase Moskal <chasemoskal@gmail.com>
2022-09-20 15:58:32 -0700

    tweak: imports

--- s/features/store2/api/services/subscription-shopping-service.ts
+++ s/features/store2/api/services/subscription-shopping-service.ts
@@ -3,12 +3,12 @@ import * as renraku from "renraku"
 
 import {StoreServiceOptions} from "../types.js"
 import {SubscriptionDetails} from "../../types/store-concepts.js"
-import {automateArgumentValidationForTierId} from "./shopping/automate-argument-validation-for-tier-id.js"
 import {cancelStripeSubscription} from "./shopping/cancel-stripe-subscription.js"
 import {uncancelStripeSubscription} from "./shopping/uncancel-stripe-subscription.js"
 import {fetchAllSubscriptionDetails} from "./shopping/fetch-all-subscription-details.js"
-import {verifyPlanHasExistingStripeSubscription} from "./shopping/verify-plan-has-existing-stripe-subscription.js"
 import {prepareToBuyStripeSubscription} from "./shopping/prepare-to-buy-stripe-subscription.js"
+import {automateArgumentValidationForTierId} from "./shopping/automate-argument-validation-for-tier-id.js"
+import {verifyPlanHasExistingStripeSubscription} from "./shopping/verify-plan-has-existing-stripe-subscription.js"
 
 export const makeSubscriptionShoppingService = (
 	options: StoreServiceOptions
