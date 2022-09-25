0842344ed6253dba91878db0199a3bb0fa6f2084
Chase Moskal <chasemoskal@gmail.com>
2022-09-25 12:05:04 -0700

    WIP: reworking for multi-pricings

--- s/features/store2/api/services/shopping/fetch-all-subscription-details.ts
+++ s/features/store2/api/services/shopping/fetch-all-subscription-details.ts
@@ -6,27 +6,43 @@ import {SubscriptionDetails} from "../../../types/store-concepts.js"
 import {getStripeId} from "../../../stripe/liaison/helpers/get-stripe-id.js"
 import {determineSubscriptionStatus} from "../helpers/utils/determine-subscription-status.js"
 
-export async function fetchAllSubscriptionDetails(
-		auth: StoreCustomerAuth
-	) {
+export async function fetchAllSubscriptionDetails({
+		storeDatabase,
+		stripeCustomerId,
+		stripeLiaisonAccount,
+	}: StoreCustomerAuth) {
 
-	const stripeSubscriptions = await auth.stripeLiaisonAccount
-		.subscriptions.list({customer: auth.stripeCustomerId})
+	const stripeSubscriptions = await stripeLiaisonAccount
+		.subscriptions
+		.list({customer: stripeCustomerId})
 
-	if(stripeSubscriptions) {
-		const subscriptionDetails = <SubscriptionDetails[]>[]
-		for (const subscription of stripeSubscriptions.data) {
+	if (!stripeSubscriptions)
+		return []
+
+	return Promise.all(
+		stripeSubscriptions.data.map(async subscription => {
 			const [stripePriceId] = subscription
-				.items.data.map(item => getStripeId(item.price))
-			const tierRow = await auth.storeDatabase.tables.subscriptions
-				.tiers.readOne(dbmage.find({stripePriceId}))
-			subscriptionDetails.push({
+				.items
+				.data
+				.map(item => getStripeId(item.price))
+
+			const price = await stripeLiaisonAccount
+				.prices
+				.retrieve(stripePriceId)
+
+			const tierRow = await storeDatabase
+				.tables
+				.subscriptions
+				.tiers
+				.readOne(dbmage.find({
+					stripeProductId: getStripeId(price.product)
+				}))
+
+			return <SubscriptionDetails>{
 				status: determineSubscriptionStatus(subscription),
 				planId: tierRow.planId.string,
 				tierId: tierRow.tierId.string,
-			})
-		}
-		return subscriptionDetails
-	}
-	else return []
+			}
+		})
+	)
 }
