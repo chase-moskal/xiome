f2166ddd5fb859c3fd709548ded1bb5aa72ac39c
Chase Moskal <chasemoskal@gmail.com>
2022-10-10 16:07:09 -0700

    restructure: test suites

---
+++ s/features/store/testing/suites/subscriptions/catalog.test.ts
@@ -0,0 +1,15 @@
+
+import {expect} from "cynic"
+import {suite} from "../../../../../types/suite.js"
+
+export default suite({})
+
+// "subscription purchases": {
+// 	"a user with regular permissions": {
+// 		async "can purchase a subscription, with an existing payment method"() {},
+// 		async "can purchase a subscription, while providing a new payment method"() {},
+// 		async "can cancel and uncancel a subscription"() {},
+// 		async "can upgrade a subscription to a higher tier"() {},
+// 		async "can downgrade a subscription to a lower tier"() {},
+// 	},
+// },
---
+++ s/types/suite.ts
@@ -0,0 +1,6 @@
+
+import {Suite} from "cynic"
+
+export function suite<S extends Suite>(s: S) {
+	return s
+}
