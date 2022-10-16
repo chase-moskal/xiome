666079b66913ef914bde64c5a8c60600a9d4cda5
Paul_Aroo <timmypaul2006@gmail.com>
2022-10-16 04:18:35 +0100

    wip: subscription catalogue tests

--- s/features/store/frontend/model/submodels/subscriptions.ts
+++ s/features/store/frontend/model/submodels/subscriptions.ts
@@ -42,6 +42,7 @@ export function makeSubscriptionsSubmodel({
 					.listing
 					.listPlans(),
 			})
+		debugger
 
 			if (get.is.userLoggedIn) {
 				await ops.operation({
--- s/features/store/testing/suites/subscriptions/catalog.test.ts
+++ s/features/store/testing/suites/subscriptions/catalog.test.ts
@@ -1,6 +1,7 @@
 
 import {expect} from "cynic"
 import {suite} from "../../../../../types/suite.js"
+import {SubscriptionStatus} from "../../../isomorphic/concepts.js"
 import {storeWithSubscriptionPlans} from "../../utils/common-setups.js"
 
 export default suite({
@@ -22,14 +23,44 @@ export default suite({
 
 		const plansForClerk = clerk.store.get.subscriptions.plans
 		expect(plansForClerk.length).equals(1)
-	
 	},
 	
 	"subscription purchases": {
 		"a user with regular permissions": {
 			async "can purchase a subscription, with an existing payment method"() {},
 			async "can purchase a subscription, while providing a new payment method"() {},
-			async "can cancel and uncancel a subscription"() {},
+			async "can cancel and uncancel a subscription"() {
+				const {api, store} = await storeWithSubscriptionPlans()
+				function getFirstTier() {
+					const plans = store.get.subscriptions.plans
+					const [plan] = plans
+					const [tier] = plan.tiers
+					return tier
+				}
+				const {tierId, pricing} = getFirstTier()
+				const {stripePriceId} = pricing[0]
+				console.log(stripePriceId)
+				const customer
+					= await api
+					.client(api.roles.customer)
+					.then(x => x.browserTab())
+
+				await customer.store.subscriptions.purchase({stripePriceId})
+				// await customer.store.refresh()
+				// await customer.store.load()
+
+				const subscription = customer.store.get
+					.subscriptions
+					.mySubscriptionDetails
+					.find(subscription => subscription.tierId === tierId)
+
+				expect(subscription.status).equals(SubscriptionStatus.Active)
+				await customer.store.subscriptions.cancel(tierId)
+				expect(subscription.status).equals(SubscriptionStatus.Cancelled)
+				await customer.store.subscriptions.uncancel(tierId)
+				expect(subscription.status).equals(SubscriptionStatus.Active)
+
+			},
 			async "can upgrade a subscription to a higher tier"() {},
 			async "can downgrade a subscription to a lower tier"() {},
 		},
